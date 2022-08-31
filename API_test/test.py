b3e9b6823f7d51151ea8740ad44e7f6a87b239ed
nadenokk <borisova_npav@mail.ru>
2022-08-31 15:14:47 +0300

    API version 1

---
+++ API_test/test.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+class JsTestTask():
+    def __init__(self, my_response_data):
+        self.my_response_data = my_response_data
+        self.namelist = []
+        self.imagelist = []
+        self.pricelist = []
+        for item in my_response_data: #преобразовываю "name", "image", "price" в отдельные объекты (разложила по листам)
+            self.namelist.append(item['name'])
+            self.imagelist.append(item['image'])
+            self.pricelist.append(item['price'])
+
+    def text_check_alcatel(self):
+        for item in self.namelist:
+            assert "Alcatel" in item
+
+    def text_check_sorted(self):
+        assert self.namelist == sorted(self.namelist)
+
+
+class Tests:
+    def test_response(self):
+        query_params = {"search": "Alcatel", "sort_field": "name"}
+        response=requests.get("https://www.lenvendo.ru/api/js-test-task/", params=query_params)
+        assert response.status_code == 200
+        #print(response.text)
+
+    def test_check_alcatel(self):
+        query_params = {"search": "Alcatel", "sort_field": "name"}
+        response = requests.get("https://www.lenvendo.ru/api/js-test-task/", params=query_params)
+        all_response_data = json.loads(response.text)
+        my_response_data = all_response_data['products']
+        text = JsTestTask(my_response_data)
+        text.text_check_alcatel()
+        text.text_check_sorted()
+
+
+
+
+
+
+
+
+
+#f"{BASE_URL}/products"
